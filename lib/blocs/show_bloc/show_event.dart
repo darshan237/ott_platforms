31841676f1d3ab7d90e70b549e1b643ebe7588fd
viral110 <68499364+viral110@users.noreply.github.com>
2025-11-02 14:17:16 +0530

    Refactor ShowEvent and add LoadMoreVideos
    Refactor ShowEvent class to extend Equatable for value comparison and add LoadMoreVideos event.

--- lib/blocs/show_bloc/show_event.dart
+++ lib/blocs/show_bloc/show_event.dart
@@ -1,20 +1,24 @@
 part of 'show_bloc.dart';
 
-abstract class ShowEvent {}
+abstract class ShowEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 class LoadVideos extends ShowEvent {
   final List<String> urls;
   LoadVideos(this.urls);
 }
 
+class LoadMoreVideos extends ShowEvent {
+  final Future<List<String>> Function() fetchMore;
+
+  LoadMoreVideos(this.fetchMore);
+}
+
 class PageChanged extends ShowEvent {
   final int index;
   PageChanged(this.index);
-}
-
-class TogglePlayPause extends ShowEvent {
-  final int index;
-  TogglePlayPause(this.index);
 }
 
 class DisposeAll extends ShowEvent {}
